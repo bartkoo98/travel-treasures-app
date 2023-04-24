17e180c078b03f85cc0c11868d3a93eff4d62177
bartkoo98 <bartosztomczuk12@gmail.com>
2023-04-25 00:45:17 +0200

    bug fix - duplication

--- src/main/java/com/github/bartkoo98/traveltreasuresfinal/TravelTreasuresFinalApplication.java
+++ src/main/java/com/github/bartkoo98/traveltreasuresfinal/TravelTreasuresFinalApplication.java
@@ -2,6 +2,7 @@ package com.github.bartkoo98.traveltreasuresfinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 
